5624090b6102cfd1bdf451cb02b6aa36d79dc2a8
joyogbu <obyjoy@yahoo.com>
2023-05-07 16:39:55 +0000

    first commit of all files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,7 +16,7 @@ def close_db(error):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ displays a HTML page with a list of states """
+    """ displays an HTML page with a list of states """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
